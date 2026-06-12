6ee78316421f3539f14c196a05acf038dae29837
Shivam Khanduri <114293283+shivamKhanduri2001@users.noreply.github.com>
2026-06-12 15:57:50 +0530

    Fix SQL syntax for dropping constraints and update primary key

--- Database of video game characters.js
+++ Database of video game characters.js
@@ -64,4 +64,5 @@ UPDATE characters SET homeland = 'Koopa Kingdom' WHERE name = 'Bowser';
 SELECT * FROM characters;
 SELECT *FROM characters ORDER BY name ASC;
 ALTER TABLE characters ADD PRIMARY KEY (name);
-ALTER TABLE characters DROP CONSTRAINTS characters_pkey;
+ALTER TABLE characters DROP CONSTRAINT characters_pkey;
+ALTER TABLE characters ADD PRIMARY KEY (character_id);
