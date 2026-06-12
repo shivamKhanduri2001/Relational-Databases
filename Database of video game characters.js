4750b4e586cf4feabb78a2bfd664efd22d7b5b38
Shivam Khanduri <114293283+shivamKhanduri2001@users.noreply.github.com>
2026-06-12 06:33:22 +0530

    Update Database of video game characters.js

--- Database of video game characters.js
+++ Database of video game characters.js
@@ -47,5 +47,6 @@ VALUES (3, 'Peach', 'Mushroom Kingdom', 'Pink');
 INSERT INTO characters VALUES (4, 'Toad', 'Mushroom Kingdom', 'Blue');
 INSERT INTO characters VALUES (5, 'Bowser', 'Dark Land', 'Red');
 SELECT *FROM characters;
-INSERT INTO characters VALUES(6,'Daisy', 'Sarasaland', 'Yellow');
-INSERT INTO characters VALUES (7,'Yoshi', 'Dinosaur Land', 'Green');
+INSERT INTO characters VALUES(6, 'Daisy', 'Sarasaland', 'Yellow');
+INSERT INTO characters VALUES (7, 'Yoshi', 'Dinosaur Land', 'Green');
+SELECT * FROM characters;
