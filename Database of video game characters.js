f8adfb0793349cfb9731ad1565c6794d9f61364e
Shivam Khanduri <114293283+shivamKhanduri2001@users.noreply.github.com>
2026-06-12 06:39:21 +0530

    Update Database of video game characters.js

--- Database of video game characters.js
+++ Database of video game characters.js
@@ -51,3 +51,4 @@ INSERT INTO characters VALUES(6, 'Daisy', 'Sarasaland', 'Yellow');
 INSERT INTO characters VALUES (7, 'Yoshi', 'Dinosaur Land', 'Green');
 SELECT * FROM characters;
 UPDATE characters SET favorite_color = 'Orange' WHERE name = 'Daisy';
+SELECT * FROM characters;
